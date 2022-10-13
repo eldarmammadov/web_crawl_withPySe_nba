b01d7f3dcc57912c8fef5cc86b59db77d18d274f
eldarmammadov <eldar_mammadov@hotmail.com>
2022-10-13 08:56:37 +0400

    gathered data from nba official stats website, is being saved to excel file(xlsx)

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # git_rep_crawl_nba
  python script for pulling off data from nba page
+I get data from website #official nba page
+	 with help of Selenium Webdriver
+I write gathered data about played games
+	to myList
+I goTo website where championship stats are
+	I choose season #2021-21
+	with Selenium click() method.
+I search for team names
+	their appropriate data stats
+	(win%, played games, points)
+	write all the data as a table 
+	to excel file with openpyxl package
+
--- w03.3.1_nba.py
+++ w03.3.1_nba.py
@@ -6,15 +6,29 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from datetime import datetime
+import openpyxl
 
-options = Options()
+wb = openpyxl.Workbook()
+ws = wb.active
+ws.title = "New Title Championship"
+
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('disable-notifications')
+chrome_options.add_argument('start-maximized')
 webdriver_service = Service(r"C:\Users\Admin\Downloads\chromedriver_win32 (1)\chromedriver.exe")
-driver = webdriver.Chrome(service=webdriver_service, options=options)
+driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
 driver.get("https://www.nba.com/schedule?pd=false&region=1")
 driver.implicitly_wait(5)
-element_to_click=driver.find_element(By.ID,"onetrust-accept-btn-handler")
-element_to_click.click()
+
+try:
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler"))).click()
+    print('accepted cookies')
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "ab-close-button"))).click()
+except Exception as e:
+    print('no cookie button!')
+
 element_to_save=driver.find_element(By.XPATH,"//div/div/div/div/h4")
 
 f=open('new_result_file00.txt','w')
@@ -48,8 +62,8 @@ f.close()
 time.sleep(1)
 driver.get("https://www.nba.com/stats/teams/traditional")
 
-season_click=wait.until(EC.visibility_of_all_elements_located((By.XPATH,"//option[@value='2021-22']")))
-season_click[0].click()
+"""season_click=wait.until(EC.visibility_of_all_elements_located((By.XPATH,"//option[@value='2021-22']")))
+season_click[0].click()"""
 
 d=1
 teamStats = []
@@ -62,11 +76,18 @@ for d in range(len(myList)):
         teamStats.append(myList[d])
         teamStats.append("new-comer-team")
 
-    print("-",teamStats)
+    #print("-",teamStats)
     for i in range(7):
         a=i+1
         teamStats.append(tr.find_element(By.XPATH, ".//td["+str(a)+"]").text)
-        print("--", teamStats)
+        ws.cell(row=int(d)+1, column=int(a)+1, value=tr.find_element(By.XPATH, ".//td[" + str(a) + "]").text)
+        #print("--", teamStats)
 
-print("---",teamStats)
+#print("---",teamStats)
+
+#adding header columns to the table
+
+
 driver.quit()
+wb.save('first_try_nba.xlsx')
+
